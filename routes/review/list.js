0f2f61e4208cd2339bf0406ef0c6c5da53fca070
wslee950920 <wslee950920@gmail.com>
2020-08-25 05:08:26 +0000

    마지막 페이지 커스텀 헤더

--- routes/review/list.js
+++ routes/review/list.js
@@ -1,12 +1,19 @@
 const { Review } = require("../../models");
 
 module.exports = async (req, res, next) => {
+  const page = parseInt(req.query.page || "1", 10);
+  if (page < 1) {
+    return res.status(400);
+  }
+
   try {
-    const reviews = await Review.findAll({
+    const reviews = await Review.findAndCountAll({
       order: [["createdAt", "DESC"]],
+      limit: 10,
+      offset: (page - 1) * 10,
     });
 
-    res.send(reviews);
+    res.set("Last-Page", reviews.count).send(reviews.rows);
   } catch (error) {
     console.error(error);
 
